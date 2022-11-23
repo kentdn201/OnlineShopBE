dd546ae00a221e73a7e69a95666ce5f4cdc6f882
Nam <admin@Nam>
2022-11-23 17:15:25 +0700

    update getOrdersDetail

--- src/main/java/com/example/springboot/service/OrderService.java
+++ src/main/java/com/example/springboot/service/OrderService.java
@@ -1,13 +1,16 @@
 package com.example.springboot.service;
 
+import com.example.springboot.config.ObjectMapperUtils;
 import com.example.springboot.dto.order.OrderDetailDto;
 import com.example.springboot.dto.order.OrderProductDto;
 import com.example.springboot.dto.order.OrderShowDto;
 import com.example.springboot.dto.order.OrderStatusDto;
+import com.example.springboot.model.OrderProduct;
 import com.example.springboot.model.exceptions.CustomException;
 import com.example.springboot.model.Enum.Role;
 import com.example.springboot.model.Order;
 import com.example.springboot.model.User;
+import com.example.springboot.repository.OrderProductRepository;
 import com.example.springboot.repository.OrderRepository;
 import com.example.springboot.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,12 +22,13 @@ import java.util.*;
 public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
-
     @Autowired
     private UserRepository userRepository;
 
     @Autowired
     private OrderProductService orderProductService;
+    @Autowired
+    private OrderProductRepository orderProductRepository;
 
     public List<Order> getAllOrders() {
         return orderRepository.findAll();
@@ -42,46 +46,17 @@ public class OrderService {
         return newOrderShowDtos;
     }
 
-    public OrderDetailDto getOrdersDetail(Integer id, Integer userId) {
-//      Lấy ra đơn hàng cần tìm
-        Optional<Order> orderDetail = orderRepository.findById(id);
-
-//      Lấy ra thông tin người dùng
-        Optional<User> existUser = userRepository.findById(userId);
-
-        List<OrderProductDto> orderProductsDtos = orderProductService.getAllOrderProductsDtos();
-        List<OrderProductDto> newOrderProductsDto = new ArrayList<>();
-        OrderDetailDto orderDetailDto = new OrderDetailDto();
-
-//      Check xem order muốn tìm có tồn tại hay không
-        if (!orderDetail.isPresent()) {
+//  Update getOrdersDetail
+    public OrderDetailDto getOrdersDetail(Integer id) {
+        Order orderDetail = orderRepository.findOrderById(id);
+        if (orderDetail == null) {
             throw new CustomException("Đơn hàng với mã: " + id + " không tồn tại");
         }
+        OrderDetailDto orderDetailDto = ObjectMapperUtils.map(orderDetail, OrderDetailDto.class);
+        List<OrderProductDto> newOrderProductsDto = ObjectMapperUtils.mapAll(orderDetail.getOrderProducts(), OrderProductDto.class);
 
-//      Check user còn tồn tại hay không và là user thì không được truy cập đến các order khác
-        if (existUser.isPresent()) {
-            if (existUser.get().getRole() == Role.User) {
-                if (orderDetail.get().getUser().getId() != userId) {
-                    throw new CustomException("Bạn không có quyền truy cập vào " + id);
-                }
-            }
-        } else {
-            throw new CustomException("Người Dùng Không Tồn Tại");
-        }
-
-        orderDetailDto.setId(orderDetail.get().getId());
-        orderDetailDto.setUserId(orderDetail.get().getUser().getId());
-        orderDetailDto.setCreateDate(orderDetail.get().getCreatedDate());
-        orderDetailDto.setOrderStatus(orderDetail.get().getOrderStatus());
-        orderDetailDto.setAddress(orderDetail.get().getAddress());
-        orderDetailDto.setTypePayment(orderDetail.get().getTypePayment());
-        orderDetailDto.setNote(orderDetail.get().getNote());
-        orderDetailDto.setPhoneNumber(orderDetail.get().getPhoneNumber());
-        for (OrderProductDto dto : orderProductsDtos) {
-            if (orderDetailDto.getId() == dto.getOrderId()) {
-                newOrderProductsDto.add(dto);
-            }
-        }
+        orderDetailDto.setCreateDate(orderDetail.getCreatedDate());
+        orderDetailDto.setUserId(orderDetail.getUser().getId());
         orderDetailDto.setOrderProductDtos(newOrderProductsDto);
         return orderDetailDto;
     }
